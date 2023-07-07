1062510207d5aff5491fc4b05db7701b28944a5a
david07201 <nighthunter0720@hotmail.com>
2023-07-07 09:48:22 +0800

    Revise class Conf and add class Generate

--- Main.py
+++ Main.py
@@ -5,8 +5,22 @@ import tkinter as tk
 from configparser import ConfigParser
 
 
-class conf():
-    def __init__(self):
+class Conf(ConfigParser):
+    def take(self):
+        i = 0
+        while i <= 1:
+            if os.path.isfile('conf.ini'):
+                try:
+                    self.read('conf.ini')
+                    break
+                except:
+                    i += 1
+                    self.create()
+            else:
+                i += 1
+                self.create()
+
+    def create(self):
         self.init = {
             'last': {
                 'date': '2020-01-01',
@@ -16,29 +30,34 @@ class conf():
                 f'{i + 1:0>3}': 0 for i in range(100)
             }
         }
-
-    def create(self):
-        print(1)
-        config = ConfigParser()
-        config.read_dict(self.init)
+        self.read_dict(self.init)
         with open('conf.ini', 'w') as f:
-            config.write(f, space_around_delimiters=True)
+            self.write(f, space_around_delimiters=True)
 
+class Generate():
+    def __init__(self):
+        self.today = datetime.date.today().strftime('%Y-%m-%d')
+        self.cd = random.randint(1, 100)
+
+    def is_lately(self, lastday):
+        return True if self.today == lastday else False
+    
+    def show(self, text):
+        pass
+
+    def go(self, config: Conf):
+        if self.islately(config['last']['date'] == self.today):
+            pass
+        else:
+            pass
 
 if __name__ == '__main__':
-    config = ConfigParser()
-    conf().create()  
-    i = 0
-    while i <= 1:
-        if os.path.isfile('conf.ini'):
-            try:
-                config.read('conf.ini')
-                print(2)
-                break
-            except:
-                i += 1
-                conf.create()
-        else:
-            i += 1
-            conf().create()            
+    config = Conf()
+    config.take()
+    generate = Generate()
+    generate.go(config)
+
+
+
+    
     
